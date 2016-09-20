cbf962ff5e9e55fee0e8bdbb78de967d1a4f53ac
Jarmo Annunen <jarmo@boulderkeskus.com>
2016-09-20 18:41:15 +0300

    Well now it works.

--- platforms/browser/www/config.js
+++ platforms/browser/www/config.js
@@ -7,6 +7,4 @@ window.ver = "1.0";
 window.fbappid = "903410899677635";
 window.api = api;
 window.state = {};
-api.apicallbase = api.server+api.api+api.version;
-$.support.cors = true;
-
+window.api.apicallbase = api.server+api.api+api.version;
--- www/my-app.js
+++ www/my-app.js
@@ -14,90 +14,6 @@ Template7.registerHelper('stringify', function (context){
 
 // Initialize your app
 var myApp = new Framework7({
-	preprocess: function (content, url, next) {
-		debugger;
-		var matches = null;
-		if (url == null) {
-			if (next != null) {
-				next(content);
-			}
-			return;
-		}
-		/* 
-		 * If a single group is being fetched, go ahead, find the group data 
-		 * via API, compile the template and since it is an AJAX call,
-		 * one has to call next() to advance in the processing 
-		 */
-		if ((matches=url.match(/problematormobile\/group\/(\d+)/i))) {
-			var groupid = matches[1];
-			var url = window.api.apicallbase + "group/";
-			$$.jsonp(url, {id : groupid}, function (data){ 
-				var compiledTemplate = Template7.compile(content);
-				var dataJSON = {group : JSON.parse(data)};
-				next(compiledTemplate(dataJSON));
-			});
-		} else if ((matches=url.match(/dashboard.html/))) {
-			$.jsonp(window.api.apicallbase+"dashinfo/?id="+Cookies.get("uid"),{},function(data) {
-				loginCheck(data);
-				debugger;
-				$.jStorage.set("grades",data.grades);
-				var compiledTemplate = Template7.compile(content);
-				var html = compiledTemplate(data);
-				next(html);
-			});
-		} else if ((matches=url.match(/problems.html/))) {
-
-			// Load problems page data and compile the template
-			//
-			var url = window.api.apicallbase + "problems/?jsonp=true";
-
-			$.jsonp(url, {}, function (data){ 
-				var compiledTemplate = Template7.compile(content);
-				var dataJSON = {walls : data};
-				next(compiledTemplate(dataJSON));
-			});
-		} else if ((matches=url.match(/invite_member.html.*?(\d+)/))) {
-			var groupid = matches[1];
-			var url = window.api.apicallbase + "group/";
-			$.jsonp(url, {id : groupid}, function (data){ 
-				var compiledTemplate = Template7.compile(content);
-				var dataJSON = {group : data};
-				next(compiledTemplate(dataJSON));
-			});
-		} else if ((matches=url.match(/list_group_members.html.*?(\d+)/))) {
-			// List group members
-			var groupid = matches[1];
-			var url = window.api.apicallbase + "list_group_members/";
-			$.jsonp(url, {id : groupid}, function (data){ 
-				var compiledTemplate = Template7.compile(content);
-				var dataJSON = {"group" : data};
-				var html = compiledTemplate(dataJSON);
-				next(html);
-			});
-
-		} else if ((matches=url.match(/problem.html.*?(\d+)/))) {
-			// List group members
-			var pid = matches[1];
-			var url = window.api.apicallbase + "problem";
-			$.jsonp(url, {id : pid}, function (data){ 
-				debugger;
-				var compiledTemplate = Template7.compile(content);
-				data.grades = $.jStorage.get("grades");
-				var html = compiledTemplate(data);
-				next(html);
-			});
-
-
-
-		} else {
-			if (content ==  null || content == "") {
-				return "";
-			}
-			var template = Template7.compile(content);
-			var resultContent = template();
-			return resultContent; 
-		}
-	},
     precompileTemplates: true,
 		template7Pages: true, // need to set this
 		modalTitle: "Problemator"
@@ -115,8 +31,9 @@ var mainView = myApp.addView('.view-main', {
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
     console.log("Device is ready!");
-		addLoginPageListeners();
+
     alert("niin ready");
+    console.log("After alert");
 });
 
 
